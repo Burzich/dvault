1ccfd682e994bb0e594539f5ca1b7b741bd37f45
danil <timerbulatov_danilka@mail.ru>
2024-10-12 16:49:36 +0300

    init

---
+++ internal/config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	LoggerLevel string `json:"logger_level" validate:"required,oneof=DEBUG INFO WARN ERROR" env:"LOGGER_LEVEL"`
+	Server      `json:"server"`
+	Postgres    `json:"postgres"`
+}
+
+type Postgres struct {
+	Addr string `json:"addr" validate:"required,url" env:"DB"`
+}
+
+type Server struct {
+	Addr string `json:"addr" validate:"required,hostname_port" env:"PORT"`
+}
+
+func Default() (Config, error) {
+	return Config{
+		LoggerLevel: "DEBUG",
+		Server:      Server{Addr: ":8080"},
+		Postgres:    Postgres{Addr: "postgres://postgres:password@localhost:5432/vault"},
+	}, nil
+}
+
+func ReadFile(fileName string) (Config, error) {
+	bytes, err := os.ReadFile(fileName)
+	if err != nil {
+		return Config{}, err
+	}
+
+	cfg := Config{}
+	if err = json.Unmarshal(bytes, &cfg); err != nil {
+		return Config{}, err
+	}
+
+	if err := validator.New().Struct(cfg); err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func ReadEnv() (Config, error) {
+	var cfg Config
+
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		return Config{}, err
+	}
+
+	if err := validator.New().Struct(cfg); err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Burzich/dvault/internal/config"
+	"github.com/jackc/pgx"
+)
+
+func main() {
+	cfg, err := config.ReadEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pgxCfg, err := pgx.ParseConnectionString(cfg.Postgres.Addr)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	connPool, err := pgx.NewConnPool(pgx.ConnPoolConfig{ConnConfig: pgxCfg})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer connPool.Close()
+
+	_, err = connPool.Exec("SELECT 1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	}))
+}
