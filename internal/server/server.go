49797cdf83fbccf7750d9536fb4f8c61d2ed02a4
danil <timerbulatov_danilka@mail.ru>
2024-10-12 23:15:58 +0300

    add routes

--- internal/server/server.go
+++ internal/server/server.go
@@ -54,6 +54,44 @@ func NewServer(addr string) *Server {
 		r.Post("/subkeys/{path}", nil)
 	})
 
+	r.Route("/auth/token", func(r chi.Router) {
+		r.Get("accessors/", nil)
+		r.Post("/create", nil)
+		r.Post("/create-orphan", nil)
+		r.Post("/create/{role_name}", nil)
+		r.Get("/lookup", nil)
+		r.Post("/lookup", nil)
+		r.Post("/lookup-accessor", nil)
+		r.Get("/lookup-self", nil)
+		r.Post("/lookup-self", nil)
+		r.Post("/renew", nil)
+		r.Post("/renew-accessor", nil)
+		r.Post("/renew-self", nil)
+		r.Post("/revoke", nil)
+		r.Post("/revoke-accessor", nil)
+		r.Post("/revoke-orphan", nil)
+		r.Post("/revoke-self", nil)
+		r.Get("/roles/", nil)
+		r.Get("/roles/{role_name}", nil)
+		r.Post("/roles/{role_name}", nil)
+		r.Delete("/roles/{role_name}", nil)
+		r.Post("/tidy", nil)
+	})
+
+	r.Route("/sys/tools", func(r chi.Router) {
+		r.Post("/hash", nil)
+		r.Post("/hash/{urlalgorithm}", nil)
+		r.Post("/random", nil)
+		r.Post("/random/{source}", nil)
+		r.Post("/random/{source}/{urlbytes}", nil)
+		r.Post("/random/{urlbytes}", nil)
+	})
+
+	r.Post("/sys/seal", nil)
+	r.Get("/sys/seal-status", nil)
+	r.Post("/sys/unseal", nil)
+	r.Get("/sys/health", nil)
+
 	srv.server.Handler = r
 
 	return srv
