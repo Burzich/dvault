47f6692d6943be8f8fb75316cb331158f1ec3252
danil <timerbulatov_danilka@mail.ru>
2024-10-20 20:46:29 +0300

    readme

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,17 +11,12 @@ import (
 type Config struct {
 	LoggerLevel string `json:"logger_level" validate:"required,oneof=DEBUG INFO WARN ERROR" env:"LOGGER_LEVEL"`
 	Server      `json:"server"`
-	Postgres    `json:"postgres"`
 	Dvault      `json:"dvault"`
 }
 
 type Dvault struct {
 	MountPath        string `json:"mount_path" validate:"required" env:"MOUNT_PATH"`
 	EncryptionMethod string `json:"encryption_method" validate:"required" env:"ENCRYPTION_METHOD"`
-}
-
-type Postgres struct {
-	Addr string `json:"addr" validate:"required,url" env:"DB"`
 }
 
 type Server struct {
@@ -32,7 +27,6 @@ func Default() (Config, error) {
 	return Config{
 		LoggerLevel: "DEBUG",
 		Server:      Server{Addr: ":8080"},
-		Postgres:    Postgres{Addr: "postgres://postgres:password@localhost:5432/vault"},
 		Dvault: Dvault{
 			MountPath:        "./data",
 			EncryptionMethod: "chacha20-poly1305",
